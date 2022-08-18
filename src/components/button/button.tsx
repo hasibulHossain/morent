76336dfd259b48796dcd610e6a1941b59e1756d8
hasibulHossain <hasibulhossain52@gmail.com>
2022-08-18 19:33:51 +0600

    update: updated button component default classes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -11,9 +11,9 @@ interface Props {
 }
 
 const Button:FC<Props> = (props: Props) => {
-    const {classes, onClick, padding = 'px-4 py-3', bgColor = 'bg-primaryblue', fontSize = 'text-xs'} = props;
+    const {classes, onClick, padding = 'px-4 py-3', bgColor = 'bg-primaryblue', fontSize = 'text-xs lg:text-base'} = props;
 
-    const combinedClasses = classnames('capitalize text-center text-xs font-medium text-white rounded-md hover:bg-blue-800 focus:outline-none', bgColor, classes, padding, fontSize)
+    const combinedClasses = classnames('capitalize text-center font-medium text-white rounded-md hover:bg-blue-800 focus:outline-none', bgColor, classes, padding, fontSize)
 
     return (
         <button onClick={onClick} className={combinedClasses}>
