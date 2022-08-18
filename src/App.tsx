fc029252e6ae8d306be6efce39ec8c1fb51e3b64
hasibulHossain <hasibulhossain52@gmail.com>
2022-08-18 12:20:41 +0600

    added react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import AllCars from "./pages/all-cars";
 import HomePage from "./pages/home-page";
 import Login from "./pages/login-page";
+import React, {useRef, useState} from "react";
+
+interface State {
+  isFilterOpen: boolean;
+  setIsFilterOpen: (currentState: boolean) => void;
+}
+
+const AppContext = React.createContext<State>({
+  isFilterOpen: true,
+  setIsFilterOpen: () => {}
+});
 
 function App() {
+  const [state, setState] = useState<State>({
+    isFilterOpen: true,
+    setIsFilterOpen: toggleIsFilterOpen
+  });
+
+  function toggleIsFilterOpen (currentState: boolean): void {
+    console.log('clicked => ', currentState)
+    setState(previousState => ({
+      ...previousState,
+      isFilterOpen: currentState,
+    }));
+  };
+
   return (
-    // <Login />
-    <HomePage />
+    <AppContext.Provider value={state}>
+      {/* // <Login /> */}
+      {/* // <HomePage /> */}
+      <AllCars />
+    </AppContext.Provider>
   )
 }
 
-export default App;
+export {App as default, AppContext};
